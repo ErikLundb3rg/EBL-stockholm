07913f36ec1acb71aa78a4265758424949862806
Erik Lundberg <erik.lumpa@hotmail.com>
2025-02-08 20:12:29 +0100

    Update backend

--- text_data.py
+++ text_data.py
@@ -23,7 +23,7 @@ def llm_summary():
     response = client.models.generate_content(
         model="gemini-2.0-flash",
         contents=f"""
-            In max 50 words, summarize what was great in the speech. Be clear and concise.
+            In max 50 words, summarize what was great in the speech. Be clear and concise. Give a concrete example from the speech.
             Then using a separator of {separator_key} do the same thing but for what could be improved.
             Refer too the speaker as 'you', also don't hallucinate. 
             {full_speech_text}
@@ -46,12 +46,21 @@ def stripped_length(data):
     offEnd = data['transcription']['utterances'][-1]['end']
     return offEnd - offStart
 
+
+def segment(start, end, value):
+    return {
+        "start": start,
+        "end": end,
+        "value": value
+    }
+
 def get_wpm_array(data, window_size=10):
     # Create array for word counts
     duration = stripped_length(data)
     start_offset = data['transcription']['utterances'][0]['start']
     num_windows = int(duration // window_size) + 1
     word_counts = [0] * num_windows
+    when = [[None, None] for _ in range(num_windows)]
     
     # Count words in each window
     for utterance in data['transcription']['utterances']:
@@ -59,15 +68,19 @@ def get_wpm_array(data, window_size=10):
             if filter_letters(word['word'].strip()):  # Only count if word has letters
                 length = len(filter_letters(word['word']) ) / 4.5
                 window_idx = int((word['start'] - start_offset) // window_size)
+                if when[window_idx][0] is None:
+                    when[window_idx][0] = word['start']
+                when[window_idx][1] = word['end']
                 word_counts[window_idx] += length
     
     # Convert to WPM (multiply by 6 since windows are 10 seconds)
-    return [int(count * 6) for count in word_counts]
+    wpms = [int(count * 6) for count in word_counts]
+    return [segment(when[i][0], when[i][1], wpms[i]) for i in range(num_windows)]
 
 def text_data():
     data = read_json_file("./transcribed.json")
-    sentences = data['transcription']['utterances']
-    sentenceAndLength = []
+    sentences = data['transcription']['sentences']
+    sentenceSegments = []
     pauseLengths = []
 
     letterCount = 0
@@ -76,20 +89,22 @@ def text_data():
     
     for sentence in sentences: 
         if previousSentence:
-            pauseLengths.append(sentence['start'] - previousSentence['end'])
-        length = sentence['end'] - sentence['start']
-        sentenceAndLength.append((sentence['text'], length))
+            pause_length = sentence['start'] - previousSentence['end']
+            pauseLengths.append(segment(previousSentence['end'], sentence['start'],round(pause_length, 1) ))
+        sentenceSegments .append(segment(sentence['start'], sentence['end'], sentence['sentence']))
         # get the pause lengths 
         previousSentence = sentence
-        letterCount += len(filter_letters(sentence['text']))
+        letterCount += len(filter_letters(sentence['sentence']))
 
     dataObject = {
-        "sentences": sentenceAndLength,
-        "shortest": min(sentenceAndLength, key=lambda x: x[1]),
-        "longest": max(sentenceAndLength, key=lambda x: x[1]),
+        "sentences": sentenceSegments,
+        "shortest": min(sentenceSegments, key=lambda x: len(x['value'])),
+        "longest": max(sentenceSegments, key=lambda x: len(x['value'])),
         "pauseLengths": pauseLengths,
         "wordsPerMinute": get_wpm_array(data),
-        "summary": llm_summary()
+        "summary": llm_summary(),
+        "averageWPM": sum([x['value'] for x in get_wpm_array(data)]) / len(get_wpm_array(data)),
+        "averagePause": sum([x['value'] for x in pauseLengths]) / len(pauseLengths)
     }
     return dataObject
 
