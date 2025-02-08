928197f3991717dbc1e39a0bc7d84583d0600f40
Erik Lundberg <erik.lumpa@hotmail.com>
2025-02-08 13:32:08 +0100

    Increase file size

--- index.py
+++ index.py
@@ -22,7 +22,7 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max file size
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024  # 1GB
 
 def allowed_file(filename):
     return '.' in filename and \
